5707f44a94bf94c0a638ad6199d64cf108e8d153
Krishan Wyse <kwysek@gmail.com>
2019-01-26 16:56:25 +0000

    Add method to calculate simple moving average

--- src/weight.rs
+++ src/weight.rs
@@ -7,16 +7,18 @@ use serde::Deserialize;
 use std::fmt;
 use std::io::Read;
 
-#[derive(Debug, Deserialize, Clone, Copy, PartialEq)]
-pub struct Weight(f64);
+#[derive(Debug, Deserialize, Clone, Copy, Eq, PartialEq)]
+pub struct Weight(u32);
 
 impl fmt::Display for Weight {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0)
+        let integer = self.0 / 10;
+        let fractional = self.0 % 10;
+        write!(f, "{}.{}", integer, fractional)
     }
 }
 
-#[derive(Debug, Deserialize, Clone, Copy)]
+#[derive(Debug, Deserialize, Clone, Copy, Eq, PartialEq)]
 pub struct WeightLogEntry {
     weight: Weight,
     timestamp: DateTime<Utc>,
@@ -74,6 +76,17 @@ impl WeightLog {
     pub fn as_slice(&self) -> &[WeightLogEntry] {
         &self.0.as_slice()
     }
+
+    fn moving_average(&self, period: usize) -> Vec<WeightLogEntry> {
+        self.0.windows(period)
+            .map(|window| {
+                let sum: u32 = window.iter().map(|entry| entry.weight().0).sum();
+                let average = sum / period as u32;
+                let last_timestamp = window[period - 1].timestamp();
+                WeightLogEntry::of(Weight(average)).at(last_timestamp)
+            })
+            .collect()
+    }
 }
 
 #[cfg(test)]
@@ -84,7 +97,7 @@ mod tests {
 
     #[test]
     fn weight_log_entries_can_be_constructed_with_just_weight() {
-        let weight = Weight(76.0);
+        let weight = Weight(760);
         let now = Utc::now();
 
         let entry = WeightLogEntry::of(weight);
@@ -95,7 +108,7 @@ mod tests {
 
     #[test]
     fn weight_log_entries_can_be_constructed_with_weight_and_timestamp() {
-        let weight = Weight(76.0);
+        let weight = Weight(760);
         let timestamp = Utc::now();
 
         let entry = WeightLogEntry::of(weight).at(timestamp);
@@ -108,14 +121,14 @@ mod tests {
     fn weight_log_entries_can_be_inserted() {
         let mut log = WeightLog::new();
 
-        log.insert(WeightLogEntry::of(Weight(76.0)));
+        log.insert(WeightLogEntry::of(Weight(760)));
 
         assert_eq!(log.len(), 1);
     }
 
     #[test]
     fn weight_log_entries_are_inserted_in_ascending_date_order() {
-        let weight = Weight(76.0);
+        let weight = Weight(760);
         let now = Utc::now();
         let older_timestamp = now - Duration::days(1);
         let mut log = WeightLog::new();
@@ -140,9 +153,9 @@ mod tests {
     fn weight_log_can_be_construct_with_valid_csv() {
         let csv = [
             "weight,timestamp",
-            "76.0,2019-01-01T00:06:00+00:00",
-            "75.0,2019-01-02T00:06:00+00:00",
-            "74.0,2019-01-03T00:06:00+00:00",
+            "760,2019-01-01T00:06:00+00:00",
+            "750,2019-01-02T00:06:00+00:00",
+            "740,2019-01-03T00:06:00+00:00",
         ].join("\n");
         let reader = BufReader::new(csv.as_bytes());
 
@@ -154,9 +167,9 @@ mod tests {
     #[test]
     fn weight_log_cannot_be_constructed_with_csv_missing_header_fields() {
         let csv = [
-            "76.0,2019-01-01T00:06:00+00:00",
-            "75.0,2019-01-02T00:06:00+00:00",
-            "74.0,2019-01-03T00:06:00+00:00",
+            "760,2019-01-01T00:06:00+00:00",
+            "750,2019-01-02T00:06:00+00:00",
+            "740,2019-01-03T00:06:00+00:00",
         ].join("\n");
         let reader = BufReader::new(csv.as_bytes());
 
@@ -169,7 +182,7 @@ mod tests {
     fn weight_log_cannot_be_constructed_with_invalid_weight_values() {
         let csv = [
             "weight,timestamp",
-            "76.0.0,2019-01-01T00:06:00+00:00",
+            "760.0,2019-01-01T00:06:00+00:00",
         ].join("\n");
         let reader = BufReader::new(csv.as_bytes());
 
@@ -182,12 +195,30 @@ mod tests {
     fn weight_log_cannot_be_constructed_with_invalid_timestamp_values() {
         let csv = [
             "weight,timestamp",
-            "76.0,2019-01-01T00:06:00+00:00:00",
+            "760,2019-01-01T00:06:00+00:00:00",
         ].join("\n");
         let reader = BufReader::new(csv.as_bytes());
 
         let log = WeightLog::from_csv(reader);
 
         assert!(log.is_err());
+    }
+
+    #[test]
+    fn weight_log_calculates_the_moving_average() {
+        let first_entry = WeightLogEntry::of(Weight(760))
+            .at(Utc::now() - Duration::days(3));
+        let second_entry = WeightLogEntry::of(Weight(757))
+            .at(Utc::now() - Duration::days(2));
+        let third_entry = WeightLogEntry::of(Weight(753))
+            .at(Utc::now() - Duration::days(1));
+        let log = WeightLog(vec![first_entry, second_entry, third_entry]);
+
+        let moving_average = log.moving_average(2);
+
+        assert_eq!(moving_average[0],
+                   WeightLogEntry::of(Weight(758)).at(second_entry.timestamp()));
+        assert_eq!(moving_average[1],
+                   WeightLogEntry::of(Weight(755)).at(third_entry.timestamp()));
     }
 }
