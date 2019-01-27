0baed414f6981c23e38b50344c6fc95a1ed96366
Krishan Wyse <kwysek@gmail.com>
2019-01-27 22:39:38 +0000

    Create parent dirs if necessary when persisting

--- src/persistence.rs
+++ src/persistence.rs
@@ -1,18 +1,41 @@
-use failure::Error;
+use failure::{Fail, Error};
 
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 struct FileOnDisk;
 
 impl FileOnDisk {
     fn write(contents: &[u8], path: impl AsRef<Path>) -> Result<(), Error> {
+        create_parent_dirs_if_needed(&path);
+
         File::create(path)?
             .write_all(contents)?;
 
         Ok(())
     }
+}
+
+#[derive(Debug, Fail)]
+enum FileOnDiskError {
+    #[fail(display = "parent directory does not exist for path: {}", path)]
+    ParentDirDoesNotExist {
+        path: String,
+    }
+}
+
+fn create_parent_dirs_if_needed(path: impl AsRef<Path>) -> Result<(), Error> {
+    let parent_dir = path.as_ref().parent()
+        .ok_or(FileOnDiskError::ParentDirDoesNotExist {
+            path: path.as_ref().to_string_lossy().to_string()
+        })?;
+
+    if !parent_dir.exists() {
+        fs::create_dir_all(parent_dir)?;
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -33,5 +56,16 @@ mod tests {
         let mut written_file = File::open(path).unwrap();
         written_file.read_to_string(&mut written_contents);
         assert_eq!(written_contents, contents);
+    }
+
+    #[test]
+    fn parent_dirs_are_created_if_they_do_not_exist() {
+        let mut path = env::temp_dir();
+        path.push("dir");
+        path.push("file.txt");
+
+        FileOnDisk::write(&[0_u8], &path).unwrap();
+
+        assert!(File::open(path).is_ok());
     }
 }
